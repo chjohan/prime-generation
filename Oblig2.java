d334eac7e5125968d213e703e324d0ddcde96895
Christian Johansen <chjohan@gmail.com>
2016-04-04 20:38:15 +0200

    Second commit

--- Oblig2.java
+++ Oblig2.java
@@ -1,9 +1,16 @@
 import java.util.*;
 
 public class Oblig2 {
+        /**
+        * Handles arguments and starts the show.
+        * 
+        * @param    arg[0]  [number] upper limit of primenumbers
+        * @param    arg[1]  [char] 'p' for parallel version, 's' for sequential version.
+        */
 	public static void main(String[] args) {
 		int maxNum = 0;
 		String seqOrPara;
+                
 		if(args.length==2) {
 			maxNum = Integer.parseInt(args[0]);
 			seqOrPara = args[1];
@@ -17,7 +24,6 @@ public class Oblig2 {
 			} else if(!seqOrPara.equals("s") && !seqOrPara.equals("p")) {
 				System.out.println("Second argument must eiter be 's' or 'p'. 'S' for sequential execution or 'p' for parallell execution.");
 			}
-		
 		} else {
 			System.out.println("You must run this program with two argumets:");
 			System.out.println("Ex. java Oblig2 100 s");
